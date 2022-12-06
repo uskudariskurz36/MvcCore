e4f0f34053257486e7571c4d572440b18c409722
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-06 10:44:07 +0300

    .

--- WebApplication2_NoteApp/WebApplication2_NoteApp/Controllers/AccountController.cs
+++ WebApplication2_NoteApp/WebApplication2_NoteApp/Controllers/AccountController.cs
@@ -4,8 +4,36 @@ namespace WebApplication2_NoteApp.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string username, string password, string repassword)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                ViewData["err-username"] = "Username is required.";
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                ViewData["err-password"] = "Password is required.";
+            }
+
+            if (string.IsNullOrEmpty(repassword))
+            {
+                ViewData["err-repassword"] = "Re-Password is required.";
+            }
+
+            if (password != repassword)
+            {
+                ViewData["err-repassword"] = "Password and re-password does not match.";
+            }
+
+
             return View();
         }
     }
