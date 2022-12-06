19b2d7b94db7a74384a4844ff43c414287a63e81
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-06 12:00:12 +0300

    .

--- WebApplication2_NoteApp/WebApplication2_NoteApp/Controllers/AccountController.cs
+++ WebApplication2_NoteApp/WebApplication2_NoteApp/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using CheckPasswordStrength;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2_NoteApp.Helpers;
 
 namespace WebApplication2_NoteApp.Controllers
 {
@@ -14,33 +15,50 @@ namespace WebApplication2_NoteApp.Controllers
         [HttpPost]
         public IActionResult Register(string username, string password, string repassword)
         {
+            bool valid = true;
+
             if (string.IsNullOrEmpty(username))
             {
                 ViewData["err-username"] = "Username is required.";
+                valid = false;
             }
 
             if (string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(password))
             {
                 ViewData["err-password"] = "Password is required.";
+                valid = false;
             }
             else
             {
                 StrengthProperty checkPass = ClassifyStrength.PasswordStrength(password);
                 ViewData["err-password-strength-text"] = checkPass.Value;
                 ViewData["err-password-strength"] = checkPass.Id;
+
+                if (checkPass.Id == 0)
+                {
+                    valid = false;
+                }
             }
 
             if (string.IsNullOrEmpty(repassword))
             {
                 ViewData["err-repassword"] = "Re-Password is required.";
+                valid = false;
             }
 
             if (password != repassword)
             {
                 ViewData["err-repassword"] = "Password and re-password does not match.";
+                valid = false;
             }
 
-          
+            if (valid)
+            {
+                UserManager userManager = new UserManager();
+                bool done = userManager.AddUser(username, password);
+
+                ViewData["done"] = done;
+            }
 
             return View();
         }
---
+++ WebApplication2_NoteApp/WebApplication2_NoteApp/Helpers/UserManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace WebApplication2_NoteApp.Helpers
+{
+    public class UserManager
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+
+        public UserManager()
+        {
+            connection = new SqlConnection("Server=.;Database=Z36NoteAppDB;Trusted_Connection=true");
+            command = connection.CreateCommand();
+        }
+
+        public bool AddUser(string username, string password)
+        {
+            command.CommandText = "INSERT INTO Users(Name,Surname,Username,Password) VALUES(NULL,NULL,@username,@password)";
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@password", password);
+
+            connection.Open();
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+
+            return result > 0;
+        }
+    }
+}
