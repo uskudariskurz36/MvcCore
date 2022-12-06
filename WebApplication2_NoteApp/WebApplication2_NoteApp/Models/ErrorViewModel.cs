e6562f4b623a8d986baf6a057dd9dd9e9b41e7aa
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-06 10:03:47 +0300

    WebApplication2_NoteApp

---
+++ WebApplication2_NoteApp/WebApplication2_NoteApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication2_NoteApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
