68fc36d6ee84770be0deeda2fc7fb401a2df58c2
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-08 14:02:14 +0300

    .

--- WebApplication2_NoteApp/WebApplication2_NoteApp/Models/ProfileModel.cs
+++ WebApplication2_NoteApp/WebApplication2_NoteApp/Models/ProfileModel.cs
@@ -11,5 +11,16 @@ namespace WebApplication2_NoteApp.Models
         [Display(Name = "Soyad")]
         [MaxLength(50)]
         public string Surname { get; set; }
+
+        public bool IsUpdatePassword { get; set; }
+
+        [Display(Name = "Şifre")]
+        [MinLength(6), MaxLength(16)]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre")]
+        [MinLength(6), MaxLength(16)]
+        [Compare(nameof(Password))]
+        public string RePassword { get; set; }
     }
 }
